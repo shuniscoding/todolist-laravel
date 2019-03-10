68b7b720fb28a3f76b71158a2eb0ccdd66968677
shun <shunichiroid@gmail.com>
2019-03-10 16:55:28 +0900

    Todos Index Page

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Todolist</title>
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+    <footer id="footer" class="text-center">
+        <p>Copyright &copy; 2019 Todolist</p>
+    </footer>
+</body>
+</html>
