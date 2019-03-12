3463183ee55ce9df3ca2768e76a5866374bacb8c
shun <shunichiroid@gmail.com>
2019-03-12 22:52:33 +0900

    Delete Todos

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -101,6 +101,9 @@ class TodosController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $todo = Todo::find($id);
+        $todo->delete();
+
+        return redirect('/')->with('success', 'Todo Deleted');
     }
 }
